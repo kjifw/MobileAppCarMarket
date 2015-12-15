a9ff32990b6cc192a3f79021e2c0dfc6b524a481
Angel Valkov <avalkov@mail.bg>
2015-12-15 22:36:43 +0200

    Initial client
    Initial client

---
+++ Client/MobileCarMarket/ViewModels/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿namespace MobileCarMarket.ViewModels
+{
+    using System.ComponentModel;
+
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+
+                handler(this, args);
+            }
+        }
+    }
+}
